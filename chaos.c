4e6a054326a8f8b385e1a13191091f3a3be5192e
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-05-12 13:27:24 -0400

    Create the `Game` structure.

---
+++ chaos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Game
+{
+    int vertex_count;
+    int point_count;
+    float factor;
+};
+
+int main()
+{
+    struct Game game = {3, 1000, 0.5};
+
+    return 0;
+}
